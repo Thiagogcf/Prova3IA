6f6b6d35eb56ec34fca095bab9969af7e9b604d0
Thiago <Thiagogferronato@gmail.com>
2023-06-14 09:26:00 -0300

    Commit inicial

---
+++ Resposta3.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+
+# Load the Iris dataset
+iris = load_iris()
+
+# Split the dataset into a training set and a test set
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=42)
+
+# Create a DecisionTreeClassifier for each criterion
+criteria = ["gini", "entropy"]
+models = {criterion: DecisionTreeClassifier(criterion=criterion, random_state=42) for criterion in criteria}
+
+# Train each model and evaluate its accuracy
+for criterion, model in models.items():
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy of model with {criterion} criterion: {accuracy:.2f}")
+
+
+# assuming models is your dictionary of trained models
+for criterion, model in models.items():
+    plt.figure(figsize=(12, 8))
+    plot_tree(model, filled=True, feature_names=iris.feature_names, class_names=iris.target_names)
+    plt.title(f"Decision tree using {criterion} criterion")
+    plt.show()
---
+++ main.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+
+# Carrega o conjunto de dados
+data = load_breast_cancer()
+
+# Definindo o intervalo de var_smoothing
+var_smoothing_range = np.linspace(1e-11, 1e-8, 100)
+
+best_score = 0
+best_var_smoothing = 0
+
+# Procura pelo melhor var_smoothing
+for var_smoothing in var_smoothing_range:
+    classifier = GaussianNB(var_smoothing=var_smoothing)
+    scores = cross_val_score(classifier, data.data, data.target, cv=5)
+    mean_score = np.mean(scores)
+    if mean_score > best_score:
+        best_score = mean_score
+        best_var_smoothing = var_smoothing
+
+print(f'O melhor valor para var_smoothing é {best_var_smoothing} com uma acurácia média de {best_score}.')
